bc2b04949da2337ce80ebc45f14538fc3bf61566
Alexis Perumal <>
2020-03-23 11:36:12 -0700

    app.js reads in CSV and does a simple line plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,64 @@
+// UCSD Data Science Bootcamp, HW16 D3
+// Alexis Perumal, 3/22/20
+
 // @TODO: YOUR CODE HERE!
+var svgWidth = 1000;
+var svgHeight = 500;
+
+// create an SVG element
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Load csv data
+d3.csv("assets/data/data.csv").then(function(stateData) {
+
+  console.log(stateData);
+
+  // cast the data from the csv as numbers
+  stateData.forEach(function(data) {
+    data.id = +data.id;
+    data.poverty = +data.poverty;
+    data.povertyMoe = +data.povertyMoe;
+    data.age = +data.age;
+    data.ageMoe = +data.ageMoe;
+    data.income = +data.income;
+    data.incomeMoe = +data.incomeMoe;
+    data.healthcare = +data.healthcare;
+    data.healthcareLow = +data.healthcareLow;
+    data.healthcareHigh = +data.healthcareHigh;
+    data.obesity = +data.obesity;
+    data.obesityLow = +data.obesityLow;
+    data.obesityHigh = +data.obesityHigh;
+    data.smokes = +data.smokes;
+    data.smokesLow = +data.smokesLow;
+    data.smokesHigh = +data.smokesHigh;
+  });
+
+  // Create a scale for your independent (x) coordinates
+  var xScale = d3.scaleLinear()
+    .domain(d3.extent(stateData, d => d.id))
+    .range([0, svgWidth]);
+
+  // Create a scale for your dependent (y) coordinates
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.poverty)])
+    .range([svgHeight, 0]);
+
+  // create a line generator function and store as a variable
+  // use the scale functions for x and y data
+  var createLine = d3.line()
+    .x(data => xScale(data.id))
+    .y(data => yScale(data.poverty));
+
+  // Append a path element to the svg, make sure to set the stroke, stroke-width, and fill attributes.
+  svg.append("path")
+    .attr("stroke", "black")
+    .attr("stroke-width", "1")
+    .attr("fill", "none")
+    .attr("d", createLine(stateData));
+
+}).catch(function(error) {
+  console.log(error);
+});
